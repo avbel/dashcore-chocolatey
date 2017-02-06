97cbf32082c7f086e4cc39728326b16e88253416
avbel <belchikovs@gmail.com>
2017-02-06 14:41:48 +0300

    Fixed found issues

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $version    = '0.12.1.0'
 $packageName= 'dashcore'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://github.com/dashpay/dash/releases/download/v$($version)/dash-$($version)-win32-setup.exe"
-$url64      = "https://github.com/dashpay/dash/releases/download/v$($version)/dash-$($version)-win64-setup.exe"
+$url        = "https://github.com/dashpay/dash/releases/download/v$($version)/dashcore-$($version)-win32-setup.exe"
+$url64      = "https://github.com/dashpay/dash/releases/download/v$($version)/dashcore-$($version)-win64-setup.exe"
 $checksum   = '2980b0ec4dfef414ef58dc0d8d6eb0bd01c182bfef88692d1dff83d6926c183f'
 $checksum64 = 'f8988703f55a0a5e9f6286b0f25fdf51b792a7f08d6aaafe64e26ec0c4747dbe'
 
